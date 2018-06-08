38358036c5b51469e25fc9bb30aeeaaf43da2c61
pacluke <lucasflores77@gmail.com>
2018-06-08 14:54:56 -0300

    description

--- include/aux2.h
+++ include/aux2.h
@@ -122,54 +122,154 @@ extern RECORDS_HANDLE *DIRECTORIES;
 extern int INIT;
 /* ------------------------ */
 
+/*************************************
+	Inicia o superbloco
+*************************************/
 int init_superblock();
 
+/*************************************
+	Inicia o bloco atual
+*************************************/
 int init_current_block();
 
+/*************************************
+	Inicia o inode do diretório raiz
+*************************************/
 int init_root_i_node();
 
+/*************************************
+	Inicia o inode de trabalho
+*************************************/
 int init_current_i_node();
 
+/*************************************
+	Inicia as listas de diretórios
+	e arquivos abertos.
+*************************************/
 int init_records_list();
 
+/*************************************
+	Carrega o superbloco
+*************************************/
 int load_superblock();
 
+/*************************************
+	Inicia todas as estruturas
+	de dados
+*************************************/
 int init_all();
 
+/*************************************
+	Imprime as impressões do
+	superbloco
+*************************************/
 void print_superblock();
 
+/*************************************
+	Recebe o número de um bloco
+	em block e carrega esse bloco
+	em CURRENT_BLOCK
+*************************************/
 int load_block(int block);
 
+/*************************************
+	Recebe um numero de inode em
+	i_node_n e carrega o inode em
+	i_node
+*************************************/
 int get_i_node(int i_node_n, struct t2fs_inode *i_node);
 
+/*************************************
+	Imprime na tela o conteúdo de i_node
+*************************************/
 void print_i_node(struct t2fs_inode *i_node);
 
+/*************************************
+	Imprime na tela o conteúdo de record
+*************************************/
 void print_record(struct t2fs_record *record);
 
+/*************************************
+	Lê e imprime na tela os records
+	contidos no inode dir
+*************************************/
 int read_i_node_content(struct t2fs_inode *dir);
 
+/*************************************
+	Devolve o primeiro diretório de
+	path
+*************************************/
 char *head_dir(char *path);
 
+/*************************************
+	Devolve path sem o priemiro
+	diretório
+*************************************/
 char *tail_dir(char *path);
 
+/*************************************
+	Encontra no diretório apontado por
+	dir_inode o arquivo de nome filename
+*************************************/
 struct t2fs_record *find_file(struct t2fs_inode *dir_inode, char* filename);
 
+/*************************************
+	Encontra no diretório apontado por
+	dir_inode o diretório de nome
+	dir_name
+*************************************/
 struct t2fs_record *find_directory(struct t2fs_inode *dir_inode, char* dir_name);
 
+/*************************************
+	Concatena a string1 com a string2
+	colocando um '/' entre elas
+*************************************/
 char *concat_dirs(char *string1, char *string2);
 
+/*************************************
+	Devolve o record apontado pelo
+	i_node_number procurando a partir do
+	inode dir.
+*************************************/
 struct t2fs_record *get_record_by_inode_number(int i_node_number, struct t2fs_inode *dir);
 
+/*************************************
+	Devole o caminho do diretório
+	raiz até o diretório apontado
+	por work_inode.
+*************************************/
 char *root_to_current(struct t2fs_inode *work_inode);
 
+/*************************************
+	Devole o primeiro bitmap livre no
+	bitmap de dados (se data_or_inode = 1)
+	ou do bitmap de inodes (se
+	data_or_inode = 0)
+*************************************/
 int get_first_free_bitmap(int data_or_inode);
 
+/*************************************
+	Usando a função write_sector
+	escreve em um bloco inteiro (4 setores)
+	o conteúdo de CURRENT_BLOCK
+*************************************/
 int write_block(int block);
 
+/*************************************
+	Inicia um inode sem informações
+*************************************/
 int init_new_inode(struct t2fs_inode *new_inode);
 
+/*************************************
+	Verifica se o nome em name já existe
+	como arquivo ou diretório.
+*************************************/
 int verify_name(char *name, int dir_or_file, struct t2fs_inode *work_dir);
 
+/*************************************
+	Inicia um bloco que não possui 
+	nada escrito.
+*************************************/
 int init_empty_block(int block);
 
 
